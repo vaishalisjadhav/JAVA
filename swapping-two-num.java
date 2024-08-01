93f74f8bf26ea21e5eccca905585d8b5982f71f5
vaishalisjadhav <vaishalijadhav9777@gmail.com>
2024-08-01 22:05:37 +0530

    Swaping

---
+++ swapping-two-num.java
@@ -0,0 +1,11 @@
+public class swapping {
+		    public static void main(String [] args){
+		        int a = 10;
+		        int b= 20;
+		        System.out.println("Before swapping \n a is " + a + "\n b is " + b);
+		        a=a+b;
+		        b=a-b;
+		        a=a-b;
+		        System.out.println("After swapping \n a is " + a + "\n b is " + b);
+	}
+}
