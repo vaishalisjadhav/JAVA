ce6d3518aae80076c3668682ec9b7b011802c914
vaishalisjadhav <vaishalijadhav9777@gmail.com>
2024-10-16 21:14:48 +0530

    for loop example

--- forloop.java
+++ forloop.java
@@ -1,7 +1,13 @@
 public class forloop {
-	public static void main(String args[]) {
-		for(int a=0; a<10; a++) {
-			System.out.println("Hello");
+	// public static void main(String args[]) {
+	// 	for(int a=0; a<10; a++) {
+	// 		System.out.println("Hello");
+	// 	}
+	// }
+
+    public static void main(String args[]) {
+		for(int a=0; a<=10; a=a+1) {
+			System.out.println(a);
 		}
 	}
 }
