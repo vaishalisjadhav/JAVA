584ea06c3b2a331a305c857f6b976afb11b2acc7
vaishalisjadhav <vaishalijadhav9777@gmail.com>
2024-10-17 17:31:00 +0530

    while loop example

--- whileloop.java
+++ whileloop.java
@@ -1,9 +1,14 @@
 public class whileloop {
 	public static void main(String args[]) {
 		int i=0;
-		while(i<=10) {
-			System.out.println("Hello");
-			i++;
+		// while(i<=10) {
+		// 	System.out.println("Hello");
+		// 	i++;
+		// }
+
+		while(i<5){
+			System.out.println("Namaste");
+			i++
 		}
 	}
 }
